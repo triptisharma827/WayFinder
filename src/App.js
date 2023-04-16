0ac2cbe8fa4bac7f848039ac799c16d60560b858
triptisharma827 <triptisharma827@gmail.com>
2023-04-16 22:01:09 +0530

    updated components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
+
+// Component Imports
 import Nav from "./components/nav";
 import Tagline from "./components/tagline";
+import Domains from "./components/domains";
+import Work from "./components/workPage";
+import Testimonial from "./components/testimonial";
+import newsletter from "./components/newsletter";
+import Footer from "./components/footer";
+
 
 function App() {
     return (
       <div>
         <Nav />
         <Tagline />
+        <Domains />
+        <Work />
+        <Testimonial />
+        <newsletter />
+        <Footer />
         
       </div>
     );
