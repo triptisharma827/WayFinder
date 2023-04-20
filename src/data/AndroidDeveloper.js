f0e8f6184a6845b717ff783b5cd28d4e2b123ee3
triptisharma827 <triptisharma827@gmail.com>
2023-04-20 22:33:02 +0530

    bug fixed

--- src/data/AndroidDeveloper.js
+++ src/data/AndroidDeveloper.js
@@ -14,37 +14,46 @@ const AndroidDeveloper={
     titleImage4: "images/techStack.png",
 
     techStack: {
-        tech1: "images/html-5.png",
-        text1: "HTML",
-        tech2: "images/html-5.png",
-        text2: "HTML",
-        tech3: "images/html-5.png",
-        text3: "HTML",
-        tech4: "images/html-5.png",
-        text4: "HTML",
-        tech5: "images/html-5.png",
-        text5: "HTML",
-        tech6: "images/html-5.png",
-        text6: "HTML"
+        tech1: "images/icons/kotlin.png",
+        text1: "Kotlin",
+        tech2: "images/icons/androidstudio.png",
+        text2: "Android Studio",
+        tech3: "images/icons/flutter.png",
+        text3: "Flutter",
+        tech4: "images/icons/firebase.png",
+        text4: "Firebase",
+        tech5: "images/icons/Rxjava.png",
+        text5: "RxJava",
+        tech6: "images/icons/jetpack.png",
+        text6: "Android Jetpack"
     },
 
     blogs:{
-        blog1: "www.blog1.com",
-        blog2: "www.blog2.com",
-        blog3: "www.blog3.com",
-        blog4: "www.blog4.com",
-        blog5: "www.blog5.com",
-        blog6: "www.blog6.com",
+        blog1: "https://kotlinlang.org/docs/home.html",
+        blog2: "https://developer.android.com/studio/intro",
+        blog3: "https://flutter.dev/",
+        blog4: "https://firebase.google.com/docs",
+        blog5: "https://www.baeldung.com/rx-java-tutorial",
+        blog6: "https://developer.android.com/jetpack",
+
+        
     }
     ,
 
     tutorials:{
-        tutorial1: "www.tutorial1.com",
-        tutorial2: "www.tutorial2.com",
-        tutorial3: "www.tutorial3.com",
-        tutorial4: "www.tutorial4.com",
-        tutorial5: "www.tutorial5.com",
-        tutorial6: "www.tutorial6.com",
+        
+        tutorial1: "KotlinConf",
+        link1: "www.youtube.com/channel/UCP7W6wo4bx4Uy6DljIiKvVA",
+        tutorial2: "Android Developers",
+        link2:"https://www.youtube.com/channel/UCVHFbqXqoYvEWM1Ddxl0QDg",
+        tutorial3: "The Net Ninja",
+        link3:"https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg",
+        tutorial4: "Firebase Developers",
+        link4: "https://www.youtube.com/channel/UCP4bf6IHJJQehibu6ai__cg",
+        tutorial5: "TechYourChance",
+        link5: "https://www.youtube.com/channel/UCO9GhodLw-MOzsN4Z4kioCw",
+        tutorial6: "Coding in Flow",
+        link6:"https://www.youtube.com/channel/UC_Fh8kvtkVPkeihBs42jGcA"
     }
 }
 
--- src/data/ArtificialIntelligence.js
+++ src/data/ArtificialIntelligence.js
@@ -14,37 +14,44 @@ title: "ARTIFICIAL INTELLIGENCE",
     titleImage4: "images/techStack.png",
 
     techStack: {
-        tech1: "images/html-5.png",
-        text1: "HTML",
-        tech2: "images/html-5.png",
-        text2: "HTML",
-        tech3: "images/html-5.png",
-        text3: "HTML",
-        tech4: "images/html-5.png",
-        text4: "HTML",
-        tech5: "images/html-5.png",
-        text5: "HTML",
-        tech6: "images/html-5.png",
-        text6: "HTML"
+        tech1: "images/icons/nlp.png",
+        text1: "Natural Language Processing",
+        tech2: "images/icons/ml.png",
+        text2: "Machine Learning",
+        tech3: "images/icons/cv.png",
+        text3: "Computer Vision",
+        tech4: "images/icons/robotics.png",
+        text4: "Robotics",
+        tech5: "images/icons/dl.png",
+        text5: "Deep Learning",
+        tech6: "images/icons/expert.png",
+        text6: "Expert Systems"
     },
 
     blogs:{
-        blog1: "www.blog1.com",
-        blog2: "www.blog2.com",
-        blog3: "www.blog3.com",
-        blog4: "www.blog4.com",
-        blog5: "www.blog5.com",
-        blog6: "www.blog6.com",
+        blog1: "https://nlp.stanford.edu/",
+        blog2: "https://www.coursera.org/learn/machine-learning",
+        blog3: "https://www.cv-foundation.org/",
+        blog4: "https://www.roboticseducationproject.org/",
+        blog5: "http://www.deeplearningbook.org/",
+        blog6: "https://www.expertsystem.com/expert-system-technology/",
     }
     ,
 
     tutorials:{
-        tutorial1: "www.tutorial1.com",
-        tutorial2: "www.tutorial2.com",
-        tutorial3: "www.tutorial3.com",
-        tutorial4: "www.tutorial4.com",
-        tutorial5: "www.tutorial5.com",
-        tutorial6: "www.tutorial6.com",
+        
+        tutorial1: "Natural Language Processing with Python",
+        link1: "https://www.youtube.com/watch?v=FLZvOKSCkxY&list=PLQVvvaa0QuDf2JswnfiGkliBInZnIC4HL",
+        tutorial2: "Machine Learning Course",
+        link2:"https://www.youtube.com/playlist?list=PLA89DCFA6ADACE599",
+        tutorial3: "Computer Vision Basics with Python and OpenCV",
+        link3:"https://www.youtube.com/playlist?list=PLWKjhJtqVAbljtmmeS0c-CEl2BIOF_suj",
+        tutorial4: "Robotics - Robot Dynamics",
+        link4: "https://www.youtube.com/playlist?list=PLMrJAkhIeNNR20Mz-VpzgfQs5zrYi085m",
+        tutorial5: "Deep Learning with PyTorch",
+        link5: "https://www.youtube.com/playlist?list=PLZbbT5o_s2xrfNyHZsM6ufI0iZENK9xgG",
+        tutorial6: "Artificial Intelligence Expert Systems",
+        link6:"https://www.youtube.com/playlist?list=PL48S46Vx6T-9NLiD7Jmn5p4uELJVgdRUf"
     }
 }
 
